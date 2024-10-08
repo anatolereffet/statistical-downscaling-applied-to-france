98d657c41010c85cff0a23f9af83ca6730209170
anatolereffet <anatole.reffet@ieseg.fr>
2024-10-08 14:54:37 +0200

    refactor: reduce CLI  noise for geopotential extraction, switch to requests

--- src/dataset_handlers/geopotential_height_extraction.py
+++ src/dataset_handlers/geopotential_height_extraction.py
@@ -1,6 +1,5 @@
 import os 
-from urllib.request import urlretrieve
-
+import requests
 import cdsapi
 import xarray as xr
 
@@ -43,38 +42,42 @@ def get_geopotential_data(download_parent_dir:str, year:str, region: Rectangular
     ]
     geopotential_feature_identifier = instanciate_mapping(geopotential_info)
 
-    era5_hourly = Era5SingleLevelsApiCall(download_parent_dir, region)
-
-    era5_hourly_request = era5_hourly._create_request(str(year), list(geopotential_feature_identifier.keys()))
-    
-    era5_hourly_request["day"] = ["01"]
-    era5_hourly_request["month"] = ["01"]
-    era5_hourly_request["time"] = ["00:00"]
-
-    era5_interim = EraInterimApiCall(download_parent_dir, region)
-    
-    era5_interim_request = era5_interim._create_request(year, f"{geopotential_feature_identifier['geopotential'].uid}.128", False)
-    era5_interim_request["date"] = f"{str(year)}-01-01"
-    era5_interim_request["time"] = "00:00"
-
-    geopotential_filepath, elevation_filepath = generate_invariant_filepaths(download_parent_dir, era5_hourly.cds_apiname, "geopotential","elevation")
+    geopotential_filepath, elevation_filepath = generate_invariant_filepaths(download_parent_dir, "reanalysis-era5-single-levels", "geopotential","elevation")
 
     if not os.path.exists(geopotential_filepath) and not os.path.exists(elevation_filepath):
-        cdsapi.Client().retrieve(era5_hourly.cds_apiname, era5_hourly_request, geopotential_filepath)
+        era5_hourly = Era5SingleLevelsApiCall(download_parent_dir, region)
+
+        era5_hourly_request = era5_hourly._create_request(str(year), list(geopotential_feature_identifier.keys()))
+        
+        era5_hourly_request["day"] = ["01"]
+        era5_hourly_request["month"] = ["01"]
+        era5_hourly_request["time"] = ["00:00"]
+
+        era5_hourly.client.retrieve(era5_hourly.cds_apiname, era5_hourly_request, geopotential_filepath)
     
-    geopotential_filepath, elevation_filepath = generate_invariant_filepaths(download_parent_dir, era5_interim.cds_apiname, "geopotential","elevation")
+    geopotential_filepath, elevation_filepath = generate_invariant_filepaths(download_parent_dir, "reanalysis-era-interim", "geopotential","elevation")
 
     if not os.path.exists(geopotential_filepath) and not os.path.exists(elevation_filepath):
-        cdsapi.Client().retrieve(era5_interim.cds_apiname, era5_interim_request, geopotential_filepath)
+        era5_interim = EraInterimApiCall(download_parent_dir, region)
+        
+        era5_interim_request = era5_interim._create_request(year, f"{geopotential_feature_identifier['geopotential'].uid}.128", False)
+        
+        era5_interim_request["date"] = f"{str(year)}-01-01"
+        era5_interim_request["time"] = "00:00"
+
+        era5_interim.client.retrieve(era5_interim.cds_apiname, era5_interim_request, geopotential_filepath)
 
     geopotential_filepath, elevation_filepath = generate_invariant_filepaths(download_parent_dir, "reanalysis-era5-land", "geopotential","elevation")
 
     if not os.path.exists(geopotential_filepath) and not os.path.exists(elevation_filepath):
         # This might break in the future, the link can be found in the documentation url below
         # https://confluence.ecmwf.int/display/CKB/ERA5-Land%3A+data+documentation#heading-Table1surfaceparametersinvariantsintime
-        urlretrieve("https://confluence.ecmwf.int/download/attachments/140385202/geo_1279l4_0.1x0.1.grib2_v4_unpack.nc?version=1&modificationDate=1591983422003&api=v2",
-                    geopotential_filepath)
-        
+        response = requests.get("https://confluence.ecmwf.int/download/attachments/140385202/geo_1279l4_0.1x0.1.grib2_v4_unpack.nc?version=1&modificationDate=1591983422003&api=v2")
+        response.raise_for_status()
+
+        with open(geopotential_filepath, 'wb') as file:
+            file.write(response.content)
+
 def get_elevation_data(download_parent_dir:str) -> None:
     """
     Transform geopotential files in geopotential height
@@ -95,7 +98,9 @@ def get_elevation_data(download_parent_dir:str) -> None:
 
         elevation = geopotential_height(geopotential_data)
 
-        elevation.to_netcdf(elevation_filepath)
+        elevation = elevation.drop_vars("expver", errors="ignore")
 
+        elevation.to_netcdf(elevation_filepath)
+        
         # Cleanup directory space
         os.remove(geopotential_filepath)
